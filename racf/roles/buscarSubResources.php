b9c4e4e1e896c98babdecb669bcde6715f9069d6
martinodg <mdgourville@gmail.com>
2020-11-23 20:05:32 +0100

    codigo de check

--- racf/roles/buscarSubResources.php
+++ racf/roles/buscarSubResources.php
@@ -50,7 +50,8 @@ include ("../../conectar7.php");
             $consultaCheckRecurso="SELECT resourcesToRolesTable.borrado FROM resourcesToRolesTable WHERE resourcesToRolesTable.id_role=$parametro1 AND resourcesToRolesTable.id_resource=$row[0];"; 
             $checkresult=mysqli_query($conexion,$consulta);
             $checkrow=mysqli_fetch_row($checkresult);
-            //pueba
+            $ischeck=$checkrow[0];
+            echo $ischeck;
             echo '<td><label class="switch"> <input type="checkbox" id="SWrecurso'.$row[0].'" name="'.$row[0].'" onclick="ABReso('.$row[0].')" > <span class="slider round"></span> </label></td>';
             echo '</tr>';
            
