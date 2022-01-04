0a42c43e6961fba34b52e90072910a1cabc67680
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-01-04 17:31:07 -0500

    query Tabla monedas

---
+++ migraciones/0401202204tabla_monedas.sql
@@ -0,0 +1,4 @@
+/*query para crear la tabla monedas*/
+
+
+CREATE TABLE monedas ( id_moneda INT NOT NULL AUTO_INCREMENT , moneda VARCHAR(20) NOT NULL , simbolo VARCHAR(10) NOT NULL , PRIMARY KEY (id_moneda)) ENGINE = InnoDB;
