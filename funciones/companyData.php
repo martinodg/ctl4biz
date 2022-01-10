33a37f175ac197b690d5dc7f2ced80416df7dab2
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-01-10 18:59:14 -0500

    metiendo datos de company y moneda en comunes.php header-footer pdf

--- funciones/companyData.php
+++ funciones/companyData.php
@@ -1,15 +1,18 @@
 <?php
-$conexion = null;
 require_once("../conectar7.php");
-
-function getCompanyData($conexion, $id_company = 0 ){
-    $query_companyData="SELECT * FROM company_data WHERE id = ".$id_company;
-    $rs_companyData=mysqli_query($conexion,$query_companyData);
-    $data =  mysqli_fetch_array($rs_companyData);
-    //@todo harcodear la ruta o completarla
-    $data['logo'] = ''.$data['logo'];
-    return $data;
+/**
+ * @param $conexion
+ * @param int $id_company
+ * @return array|null
+ */
+function getCompanyData($conexion, $id_company = 0 )
+{
+    $query_companyData="SELECT company_data.id, company_data.razon_soc, company_data.address, company_data.zip_code,company_data.cod_fiscal, company_data.leyenda, company_data.logo, monedas.simbolo, monedas.moneda FROM company_data JOIN monedas ON monedas.id_moneda = company_data.moneda_id WHERE company_data.id = ".$id_company;
+    $rs_companyData= mysqli_query($conexion,$query_companyData);
+    if(!$rs_companyData){
+        return  null;
+    }
+    return mysqli_fetch_array($rs_companyData);
 }
-
 
 $companyData = getCompanyData($conexion);
