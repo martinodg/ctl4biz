10de766a7ffb19bd9daf3e999a16a3f9d02e1e5c
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-01-11 18:02:15 -0500

    unificando query campos de company_data

---
+++ migraciones/11012022000000_campos_migracion_companydata.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `company_data` ADD `razon_soc` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `id`;
+
+ALTER TABLE `company_data` ADD `moneda_id` INT(11) NOT NULL AFTER `zip_code`, ADD `cod_fiscal` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `moneda_id`, ADD `leyenda` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `cod_fiscal`, ADD `logo` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `leyenda`;
