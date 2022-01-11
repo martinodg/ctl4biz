20be8a288932378ec5efe4b220d22fd15f948b0a
martinodg <mdgourville@gmail.com>
2022-01-11 15:11:08 +0000

    commit all dev changes

--- conectar7.php
+++ conectar7.php
@@ -1,8 +1,8 @@
 <?php
-include_once(__DIR__.DIRECTORY_SEPARATOR.'enviroment.php');
 if(session_id() == '') {
     session_start();
 }
+//include_once(__DIR__.DIRECTORY_SEPARATOR.'enviroment.php');
 
 $Servidor="database";
 $BaseDeDatos= $_SESSION["BaseDeDatos"];
