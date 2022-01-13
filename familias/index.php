77a66f644eb302077b8a006316087bc161456e6f
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-01-13 10:51:54 -0500

    Correcion en traducciones Item Family

--- familias/index.php
+++ familias/index.php
@@ -86,7 +86,7 @@ if ($cadena_busqueda<>"") {
 		<div id="pagina">
 			<div id="zonaContenido">
 				<div align="center">
-                    <div id="tituloForm" class="header"><span  id="tbtipoar">Buscar Tipo de Articulo</span></div>
+                    <div id="tituloForm" class="header"><span  id="tbtipoar">Buscar Tipos de Articulos</span></div>
 				<div id="frmBusqueda">
 				<form id="form_busqueda" name="form_busqueda" method="post" action="rejilla.php" target="frame_rejilla">
 					<table class="fuente8" width="98%" cellspacing=0 cellpadding=3 border=0>					
