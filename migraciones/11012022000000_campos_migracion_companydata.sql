5b7c6cee4dc9d2914239067af45ac2a8b622c370
webmaster <webmaster@divit.la>
2022-01-18 18:17:37 -0300

    Corrigiendo migraciones

--- migraciones/11012022000000_campos_migracion_companydata.sql
+++ migraciones/11012022000000_campos_migracion_companydata.sql
@@ -3,14 +3,17 @@ START TRANSACTION;
 -- Actualizacion de las tabla company_data
 --
 ALTER TABLE `company_data`
-    ADD `razon_soc` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `id`;
+    ADD `razon_soc` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `id`,
     ADD `moneda_id` INT(11) NOT NULL AFTER `zip_code`,
-    ADD `cod_fiscal` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `moneda_id`,
-    ADD `leyenda` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `cod_fiscal`,
-    ADD `logo` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL AFTER `leyenda`;
+    ADD `cod_fiscal` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL ,
+    ADD `leyenda` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    ADD `logo` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL ;
 
-COMMIT;
+
+
 --
 -- Poniendo las monedas en EUR
 --
 UPDATE `company_data` SET `moneda_id` = '3' WHERE `company_data`.`id` = 0;
+
+COMMIT;
