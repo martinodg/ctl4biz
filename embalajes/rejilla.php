2f7a69fd5029715f3841099c1f26ecc64d414f3d
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-02-11 11:40:26 -0500

    alineando cabeceray datos de la lista de embalaje

--- embalajes/rejilla.php
+++ embalajes/rejilla.php
@@ -63,9 +63,19 @@ $filas=mysqli_result($rs_busqueda,0,"filas");
 
 	<body onload=inicio()>	
 		<div id="pagina">
+            <div style="width: 100%; height: 33px; background-color: #251d1d;position: absolute; top: -8px;"></div>
 			<div id="zonaContenido">
 			<div align="center">
 			<table class="fuente8" width="100%" cellspacing=0 cellpadding=3 border=0 ID="Table1">
+                <tr class="cabeceraTabla">
+                    <td class="aCentro" width="12%"><span id="titem">ITEM</span></td>
+                    <td class="aCentro" width="12%"><span id="tcodio">CODIGO</span></td>
+                    <td width="30%"><span id="tnomb">NOMBRE</span></td>
+                    <td class="aCentro" width="20%"><span id="tunidad">UNIDAD DE MEDIDA</span></td>
+                    <td width="6%">&nbsp;</td>
+                    <td width="6%">&nbsp;</td>
+                    <td width="6%">&nbsp;</td>
+                </tr>
 			<input type="hidden" name="numfilas" id="numfilas" value="<? echo $filas?>">
 				<? $iniciopagina=$_POST["iniciopagina"];
 				if (empty($iniciopagina)) { $iniciopagina=$_GET["iniciopagina"]; } else { $iniciopagina=$iniciopagina-1;}
@@ -74,16 +84,22 @@ $filas=mysqli_result($rs_busqueda,0,"filas");
 					if ($filas > 0) { ?>
 						<? $sel_resultado="SELECT * FROM embalajes WHERE borrado=0 AND ".$where;
 						   $sel_resultado=$sel_resultado."  limit ".$iniciopagina.",10";
-						   $res_resultado=mysqli_query($conexion,$sel_resultado);
-						   $contador=0;
-						   while ($contador < mysqli_num_rows($res_resultado)) { 
-								 if ($contador % 2) { $fondolinea="itemParTabla"; } else { $fondolinea="itemImparTabla"; }?>
+                            $res_resultado=mysqli_query($conexion,$sel_resultado);
+
+
+                        $contador=0;
+						   while ($contador < mysqli_num_rows($res_resultado)) {
+                               $cod_unimedida = mysqli_result($res_resultado,$contador,'codunidadmedida');
+                               $query_unimedida ="SELECT * FROM unidadesmedidas WHERE codunidadmedida='$cod_unimedida'";
+                               $rs_queryUnimedida = mysqli_query($conexion,$query_unimedida);
+                               if ($contador % 2) { $fondolinea="itemParTabla"; } else { $fondolinea="itemImparTabla"; }?>
 						<tr class="<?php echo $fondolinea?>">
 							<td class="aCentro" width="12%"><? echo $contador+1;?></td>
-							<td width="20%"><div align="center"><? echo mysqli_result($res_resultado,$contador,"codembalaje")?></div></td>
-							<td width="50%"><div align="left"><? echo mysqli_result($res_resultado,$contador,"nombre")?></div></td>
+							<td width="12%"><div align="center"><? echo mysqli_result($res_resultado,$contador,"codembalaje")?></div></td>
+							<td width="30%"><div align="center"><? echo mysqli_result($res_resultado,$contador,"nombre")?></div></td>
+							<td width="20%"><div align="center"><? echo mysqli_result($rs_queryUnimedida,0,"nombre");?></div></td>
 							<td width="6%"><div align="center"><a href="#"><img src="../img/modificar.svg" width="16" height="16" border="0" onClick="modificar_embalaje(<?php echo mysqli_result($res_resultado,$contador,"codembalaje")?>)" data-ttitle="modificar" title="Modificar"></a></div></td>
-														<td width="6%"><div align="center"><a href="#"><img src="../img/ver.svg" width="16" height="16" border="0" onClick="ver_embalaje(<?php echo mysqli_result($res_resultado,$contador,"codembalaje")?>)" data-ttitle="visualizar" title="Visualizar"></a></div></td>
+							<td width="6%"><div align="center"><a href="#"><img src="../img/ver.svg" width="16" height="16" border="0" onClick="ver_embalaje(<?php echo mysqli_result($res_resultado,$contador,"codembalaje")?>)" data-ttitle="visualizar" title="Visualizar"></a></div></td>
 							<td width="6%"><div align="center"><a href="#"><img src="../img/eliminar.svg" width="16" height="16" border="0" onClick="eliminar_embalaje(<?php echo mysqli_result($res_resultado,$contador,"codembalaje")?>)" data-ttitle="eliminar" title="Eliminar"></a></div></td>
 						</tr>
 						<? $contador++;
