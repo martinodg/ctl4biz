da5f3aaa7da9890694347990a9a489e8ad19fb60
mmarecodivit <marcelo_mareco@div-it.com.ar>
2022-02-14 12:10:03 -0500

    agregando getPackaging.php

---
+++ funciones/BackendQueries/getPackaging.php
@@ -0,0 +1,38 @@
+<?php
+include "../../conectar7.php";
+
+if (isset($_GET["unidadmedida"])) {
+    $cod_unidadmedida = $_GET["unidadmedida"];
+}
+if (isset($_GET["campo"])) {
+    $campo = $_GET["campo"];
+}
+
+if (isset($campo) & isset($unidadmedida)) {
+    $query_value = "SELECT $campo FROM `embalajes` WHERE codunidadmedida='$cod_unidadmedida' AND borrado = 0;";
+    //echo $query_value;
+    $res_value = mysqli_query($conexion, $query_value);
+    $pre_value = mysqli_fetch_array($res_value);
+    $value = $pre_value[$campo];
+}
+
+$query_embalajes = "SELECT codembalaje, nombre FROM `embalajes` WHERE codunidadmedida='$cod_unidadmedida' AND borrado = 0 ORDER BY nombre ASC";
+$res_embalajes = mysqli_query($conexion, $query_embalajes);
+$nr_embalajes = mysqli_num_rows($res_embalajes);
+if ($nr_embalajes) {
+    while ($nr_embalajes > 0) {
+        $row = mysqli_fetch_row($res_embalajes);
+        if ($row[0] == $value) {
+            $selected = 'selected="selected"';
+        } else {
+            $selected = '';
+        }
+        echo '<option value="' . $row[0] . '" ' . $selected . '>' . $row[1] . '</option>';
+        $nr_embalajes--;
+    }
+} else {
+    echo '<option value="">Sin Embalajes para</option>';
+}
+?>
+
+
